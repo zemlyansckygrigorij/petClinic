65cbd966e2dc601bdb480c6823f418e78f5301c3
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-07 16:50:12 +0500

    #t3 create class VetComponentImpl

---
+++ src/main/kotlin/com/example/petclinic/db/services/VetComponentImpl.kt
@@ -0,0 +1,43 @@
+package com.example.petclinic.db.services
+
+import com.example.petclinic.db.entity.Vet
+import com.example.petclinic.db.repo.VetRepo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class VetComponentImpl @Autowired constructor(
+    val vetRepo: VetRepo
+): VetComponent {
+    override fun save(vet: Vet) {
+        vetRepo.save(vet)
+    }
+
+    override fun findById(id: Long): Vet {
+       return vetRepo.findById(id).orElseThrow { throw Exception() }
+    }
+
+    override fun findAll(): ArrayList<Vet> {
+        val vetList = ArrayList<Vet>()
+        if(vetList.addAll( vetRepo.findAll().toList())){
+            return vetList
+        }else{
+            throw Exception()
+        }
+    }
+
+    override fun deleteById(id: Long) {
+        vetRepo.deleteById(id)
+    }
+
+    override fun findByName(name: String): ArrayList<Vet> {
+        return vetRepo.findByName(name)
+    }
+
+    override fun findByPhone(phone: String): Vet {
+        vetRepo.findByPhone(phone).let{
+            return  it!!
+        }
+        throw Exception()
+    }
+}
