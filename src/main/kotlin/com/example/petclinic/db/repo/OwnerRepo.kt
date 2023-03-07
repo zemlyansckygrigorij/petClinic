6da10d28ef3739706d88531fed1d48ee1f09031b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-07 16:52:01 +0500

    #t3 change interface OwnerRepo add methods: findByPhone, findByName

--- src/main/kotlin/com/example/petclinic/db/repo/OwnerRepo.kt
+++ src/main/kotlin/com/example/petclinic/db/repo/OwnerRepo.kt
@@ -2,7 +2,25 @@ package com.example.petclinic.db.repo
 
 import com.example.petclinic.db.entity.Owner
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OwnerRepo: JpaRepository<Owner, Long>
+interface OwnerRepo: JpaRepository<Owner, Long>{
+    @Query(value
+    = "select * "
+            + "from owner  "
+            + "where full_name  LIKE %:name% ",
+        nativeQuery = true)
+    open fun findByName(@Param("name") name: String?): ArrayList<Owner>
+
+    @Query(value
+    = "select * "
+            + "from owner  "
+            + "where phone  LIKE %:phone% ",
+        nativeQuery = true)
+    open fun findByPhone(@Param("phone") phone: String?): Owner?
+
+
+}
