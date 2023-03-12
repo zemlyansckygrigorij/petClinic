ea096ba1ffc26b171e3650c66b03d25b2468f23e
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-12 14:27:05 +0500

    #t8 change class OwnerComponentImpl

--- src/main/kotlin/com/example/petclinic/db/services/OwnerComponentImpl.kt
+++ src/main/kotlin/com/example/petclinic/db/services/OwnerComponentImpl.kt
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service
 class OwnerComponentImpl @Autowired constructor(
     val  ownerRepo: OwnerRepo
 ): OwnerComponent{
-    override fun save(owner: Owner) {
-        ownerRepo.save(owner)
+    override fun save(owner: Owner): Owner {
+        return ownerRepo.save(owner)
     }
 
     override fun findById(id: Long): Owner {
