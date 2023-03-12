f9b28574fd9acaa4cee5ddcf87d35c62a6c810d5
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-12 14:26:42 +0500

    #t8 change interface OwnerComponent

--- src/main/kotlin/com/example/petclinic/db/services/OwnerComponent.kt
+++ src/main/kotlin/com/example/petclinic/db/services/OwnerComponent.kt
@@ -3,7 +3,7 @@ package com.example.petclinic.db.services
 import com.example.petclinic.db.entity.Owner
 
 interface OwnerComponent {
-    fun save(owner: Owner)
+    fun save(owner: Owner): Owner
     fun findById(id: Long) : Owner
     fun findAll(): ArrayList<Owner>
     fun deleteById(id: Long)
