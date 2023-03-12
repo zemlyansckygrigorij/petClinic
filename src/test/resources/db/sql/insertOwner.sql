e48718b5310dec722db68d4e73c0f40a687205ba
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-12 14:23:58 +0500

    #t8 create file insertOwner.sql for insert test data into table owner

---
+++ src/test/resources/db/sql/insertOwner.sql
@@ -0,0 +1,19 @@
+delete from owner where id>0;
+insert into owner(full_name, address , phone, birthday, gender)
+values('Bradley Alexander Abbe','london','23-35-2324','02-03-99','MALE'),
+      ('Martin Quinn Abbett','london','23-45-4324','02-03-99','MALE'),
+      ('Max Mendoza Alan','london','23-42-2324','02-03-99','MALE'),
+      ('Stephen Allen Alden','london','23-35-2324','02-03-99','MALE'),
+      ('Dominic Clarke Beaner','london','13-45-2324','02-03-99','MALE'),
+      ('Theobold Elliott Bear','london','23-45-2324','02-03-99','MALE'),
+      ('Davy Mills Bearfield','london','22-45-2324','02-03-99','MALE'),
+      ('Dominic Bowman Beatley','london','23-25-2324','02-03-99','MALE'),
+      ('Zachary Mason Beaver','london','23-42-2324','02-03-99','MALE'),
+      ('Keith Oliver Chace','london','23-45-1324','02-03-99','MALE'),
+      ('Manfred Webb Chadburn','london','23-45-2224','02-03-99','MALE'),
+      ('Milton Daniels Chaddick','london','23-45-2314','02-03-99','MALE'),
+      ('Robin Greenwood Chadwell','london','23-45-2322','02-03-99','MALE'),
+      ('Justin Rodriquez Chaffey','london','23-45-2354','02-03-99','MALE'),
+      ('Ford Simonds Chalkley','london','23-45-2524','02-03-99','MALE'),
+      ('Ferris Eland Chamberlain','london','23-45-4324','02-03-99','MALE'),
+      ('Rocky Black Dabler','london','23-46-2324','02-03-99','MALE');
