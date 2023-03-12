dcd008dfa2c90ad217327cabe4bd42249d804542
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-12 14:28:00 +0500

    #t8 create class VetComponentImplTest

---
+++ src/test/kotlin/com/example/petclinic/db/services/VetComponentImplTest.kt
@@ -0,0 +1,40 @@
+package com.example.petclinic.db.services
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+class VetComponentImplTest {
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun deleteById() {
+    }
+
+    @Test
+    fun findByName() {
+    }
+
+    @Test
+    fun findByPhone() {
+    }
+
+    @Test
+    fun getVetRepo() {
+    }
+}
