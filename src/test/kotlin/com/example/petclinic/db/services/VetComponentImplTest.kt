74e4874f94b0cdc41826a9fed4642ac6b63ff229
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-03-13 16:18:29 +0500

    #t8 update class VetComponentImplTest

--- src/test/kotlin/com/example/petclinic/db/services/VetComponentImplTest.kt
+++ src/test/kotlin/com/example/petclinic/db/services/VetComponentImplTest.kt
@@ -1,25 +1,54 @@
 package com.example.petclinic.db.services
 
+import com.example.petclinic.db.entity.Gender
+import com.example.petclinic.db.entity.Vet
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.text.SimpleDateFormat
+import java.util.*
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
-class VetComponentImplTest {
+@SpringBootTest
+class VetComponentImplTest @Autowired constructor(val vetComponent: VetComponent) {
 
     @Test
     fun save() {
+        val vets = vetComponent.findAll()
+        assertEquals(vets.size,17)
+        val id = vets.maxBy{it.id}.id + 1
+        val date = Date()
+        val vet = Vet(id,"owner","Address","phone",date, Gender.MALE,"first")
+        val vetSave = vetComponent.save(vet)
+        assertEquals(vetComponent.findAll().size,18)
+        val vetFrom = vetComponent.findById(vetSave.id)
+         assertEquals(vetFrom.gender, Gender.MALE )
+        assertEquals(vetFrom.fullName,"owner")
+        assertEquals(vetFrom.address,"Address")
+        assertEquals(vetFrom.phone,"phone")
+        assertEquals(vetFrom.qualification,"first")
+
+        val pattern = "yyyy-MM-dd"
+        val simpleDateFormat = SimpleDateFormat(pattern)
+        assertEquals(vetFrom.birthday.toString(),simpleDateFormat.format(date).toString())
+        vetComponent.deleteById(vetFrom.id)
+        assertEquals(vetComponent.findAll().size, 17)
     }
 
     @Test
     fun findById() {
+        val vet = vetComponent.findById(1)
+        assertEquals(vet.id, 1)
+        assertEquals(vet.gender, Gender.MALE )
+        assertEquals(vet.fullName,"Bradley Alexander Abbe")
+        assertEquals(vet.address,"Baltimore")
+        assertEquals(vet.phone,"23-35-2324")
+        assertEquals(vet.birthday.toString() ,"1990-01-30")
     }
 
     @Test
     fun findAll() {
+        assertEquals(vetComponent.findAll().size, 17)
     }
 
     @Test
@@ -28,13 +57,24 @@ class VetComponentImplTest {
 
     @Test
     fun findByName() {
+        val vetsFromTable = vetComponent.findByName("Russ")
+        assertEquals(vetsFromTable.size, 1)
+        val vet = vetsFromTable[0]
+
+        assertEquals(vet.gender, Gender.MALE )
+        assertEquals(vet.fullName,"Russ Abbot")
+        assertEquals(vet.address,"Baltimore")
+        assertEquals(vet.phone,"73-35-2324")
+        assertEquals(vet.qualification, "first" )
     }
 
     @Test
     fun findByPhone() {
-    }
-
-    @Test
-    fun getVetRepo() {
+        val vet = vetComponent.findByPhone("73-35-2324")
+        assertEquals(vet.gender, Gender.MALE )
+        assertEquals(vet.fullName,"Russ Abbot")
+        assertEquals(vet.address,"Baltimore")
+        assertEquals(vet.phone,"73-35-2324")
+        assertEquals(vet.qualification, "first" )
     }
 }
