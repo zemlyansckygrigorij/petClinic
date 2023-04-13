46aefe3c09a4211c928f740512cbe299d150754c
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-04-13 13:54:59 +0500

    #t19 update class ProducerService

--- src/main/kotlin/com/example/petclinic/transport/service/ProducerService.kt
+++ src/main/kotlin/com/example/petclinic/transport/service/ProducerService.kt
@@ -1,6 +1,7 @@
 package com.example.petclinic.transport.service
 
 import com.example.petclinic.db.entity.Owner
+import com.example.petclinic.transport.mapper.OwnerMapper
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
@@ -9,25 +10,22 @@ import org.springframework.stereotype.Service
 
 @Service
 class ProducerService @Autowired constructor(
-    val kafkaTemplate: KafkaTemplate<String, String>
+    val kafkaTemplate: KafkaTemplate<String, Owner>
 ) {
 
     fun produce(message: String) {
         println("Producing the message: $message")
-        kafkaTemplate.send("messages", message)
+        //kafkaTemplate.send("messages", message)
     }
     fun produceOwner(owner: Owner){
-        println("Producing the message: ${getOwnerJson(owner)}")
-        println("kafkaTemplate.kafkaAdmin - "+kafkaTemplate.kafkaAdmin)
-        println("kafkaTemplate.defaultTopic - "+kafkaTemplate.defaultTopic)
-        println("kafkaTemplate.messageConverter - "+kafkaTemplate.messageConverter)
-        println("kafkaTemplate.transactionIdPrefix - "+kafkaTemplate.transactionIdPrefix)
-        println("kafkaTemplate.producerFactory - "+kafkaTemplate.producerFactory)
-        println("--- - ")
-        (1..100).forEach {
-            kafkaTemplate.send("messages", it.toString())
-        }
 
-       // kafkaTemplate.send("messages", getOwnerJson(owner))
+        println("OwnerMapper().getOwnerDto(owner) - "+OwnerMapper().getOwnerDto(owner))
+
+        println("--- - ")
+       /* (1..100).forEach {
+            kafkaTemplate.send("messages", it.toString())
+        }*/
+       // OwnerMapper().getOwnerDto(owner)
+        kafkaTemplate.send("messages", owner)
     }
 }
