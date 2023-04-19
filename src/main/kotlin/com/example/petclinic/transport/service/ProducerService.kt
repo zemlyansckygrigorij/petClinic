13ef09f4c486a4867bc768464b32e266cd917b8f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-04-19 17:33:44 +0500

    #t23 update class ProducerService

--- src/main/kotlin/com/example/petclinic/transport/service/ProducerService.kt
+++ src/main/kotlin/com/example/petclinic/transport/service/ProducerService.kt
@@ -1,9 +1,13 @@
 package com.example.petclinic.transport.service
 
 import com.example.petclinic.db.entity.Owner
-import com.example.petclinic.transport.dto.OwnerDto
+import com.example.petclinic.db.entity.Pet
+import com.example.petclinic.db.entity.Services
+import com.example.petclinic.db.entity.Vet
 import com.example.petclinic.transport.mapper.OwnerMapper
-
+import com.example.petclinic.transport.mapper.PetMapper
+import com.example.petclinic.transport.mapper.ServiceMapper
+import com.example.petclinic.transport.mapper.VetMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
@@ -11,9 +15,18 @@ import org.springframework.stereotype.Service
 
 @Service
 class ProducerService @Autowired constructor(
-    val kafkaTemplate: KafkaTemplate<String, OwnerDto>
+    val kafkaTemplate: KafkaTemplate<String, Any>
 ) {
     fun produceOwner(owner: Owner){
-        kafkaTemplate.send("messages", OwnerMapper().getOwnerDto(owner))
+        kafkaTemplate.send("owners", OwnerMapper().getOwnerDto(owner))
+    }
+    fun producePet(pet: Pet){
+        kafkaTemplate.send("pets", PetMapper().getPetDto(pet))
+    }
+    fun produceService(services: Services){
+        kafkaTemplate.send("services", ServiceMapper().getServicesDto(services))
+    }
+    fun produceVet(vet: Vet){
+        kafkaTemplate.send("vets", VetMapper().getVetDto(vet))
     }
 }
