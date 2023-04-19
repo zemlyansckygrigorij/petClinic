dfd4f3d4a6ff58f9793ddf0d529ac150145e8e22
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-04-19 17:29:28 +0500

    #t22 update class PetDto

--- src/main/kotlin/com/example/petclinic/transport/dto/PetDto.kt
+++ src/main/kotlin/com/example/petclinic/transport/dto/PetDto.kt
@@ -1,4 +1,13 @@
 package com.example.petclinic.transport.dto
 
-class PetDto {
-}
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+data class PetDto (
+    @JsonProperty("name") var name: String,
+    @JsonProperty("kind") var kind: String,
+    @JsonProperty("age") var age: Int?,
+    @JsonProperty("gender") var gender: String,
+    @JsonProperty("idOwner") var idOwner: Int?
+)
