a5636594b0442db54cef778ef70db978306488f6
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-04-19 17:36:44 +0500

    #t23 create class Services

---
+++ src/main/kotlin/com/example/petclinic/db/entity/Services.kt
@@ -0,0 +1,27 @@
+package com.example.petclinic.db.entity
+
+import jakarta.persistence.*
+
+
+/**
+ * @author Grigoriy Zemlyanskiy
+ * @version 1.0
+ * class Service
+ */
+@Entity
+@Table(name = "services",schema = "public")
+class Services(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(name = "name", nullable = false)
+    val name: String,
+
+    @Column(name = " description", nullable = true)
+    val description: String? = null,
+
+    @Column(name = "price", nullable = false)
+    val price: Double
+)
