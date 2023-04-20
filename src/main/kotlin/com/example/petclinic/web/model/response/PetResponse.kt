3b751b252ccfb165d7c1ed283a41dc2078029138
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-04-20 11:56:31 +0500

    #t23 create data class PetResponse

---
+++ src/main/kotlin/com/example/petclinic/web/model/response/PetResponse.kt
@@ -0,0 +1,49 @@
+package com.example.petclinic.web.model.response
+
+import com.example.petclinic.db.entity.Pet
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+@Schema(description = "Животное")
+data class PetResponse (
+    @Schema(description = "Кличка", example = "Барбос")
+    @NotBlank
+    @Size(max = 100)
+    @JsonProperty("name") var name: String,
+
+    @Schema(description = "Вид", example = "Собака")
+    @NotBlank
+    @Size(max = 100)
+    @JsonProperty("kind") var kind: String,
+
+    @Schema(description = "Возраст", example = "14")
+    @NotBlank
+    @Size(max = 100)
+    @JsonProperty("age") var age: Int?,
+
+    @Schema(description = "Пол", example = "Самец")
+    @NotBlank
+    @Size(max = 100)
+    @JsonProperty("gender") var gender: String,
+
+    @Schema(description = "идентификатор владельца", example = "123")
+    @NotBlank
+    @Size(max = 100)
+    @JsonProperty("idOwner") var idOwner: Int?
+) {
+    companion object {
+        fun getPetResponse(pet: Pet): PetResponse? {
+            return pet.name?.let {it
+                PetResponse(
+                    it,
+                    pet.kind,
+                    pet.age,
+                    pet.gender.toString(),
+                    pet.idOwner
+                )
+            }
+        }
+    }
+}
