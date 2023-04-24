e3da8a7461c24383a0ea0a18793e6382044f2e68
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-04-24 16:39:59 +0500

    #t17 change class OwnerController add @Operation

--- src/main/kotlin/com/example/petclinic/web/controllers/OwnerController.kt
+++ src/main/kotlin/com/example/petclinic/web/controllers/OwnerController.kt
@@ -6,7 +6,6 @@ import com.example.petclinic.db.services.OwnerComponent
 import com.example.petclinic.transport.service.ProducerService
 import com.example.petclinic.web.model.request.OwnerRequest
 import com.example.petclinic.web.model.response.OwnerResponse
-import jakarta.persistence.*
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,47 +15,91 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+/*
+https://www.bezkoder.com/spring-boot-swagger-3/
+https://www.bezkoder.com/swagger-3-annotations/
+*/
 @RestController
 @RequestMapping("/owner")
+@Tag(name = "API работы с собственниками животных")
 class OwnerController(private val ownerComponent: OwnerComponent,
                       private val producerService: ProducerService
 ) {
 
     @GetMapping
     @ResponseStatus(HttpStatus.FOUND)
+    @Operation(description = "Получение всех собственников")
     fun findAll()=ownerComponent.findAll().map { owner ->OwnerResponse
         .getOwnerResponse(owner) }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.FOUND)
-    fun findById(@PathVariable(name = "id") id: Long) = OwnerResponse
+    @Operation(description = "Получение собственника по идентификатору")
+    fun findById(
+        @Parameter(description = "Идентификатор собственника", example = "68406", required = true,
+            style = ParameterStyle.DEFAULT)
+        @PathVariable(name = "id") id: Long
+    ) = OwnerResponse
         .getOwnerResponse(ownerComponent.findById(id))//ownerComponent.findById(id)
 
     @GetMapping("/name")
     @ResponseStatus(HttpStatus.FOUND)
-    fun findByName(@RequestBody name:String)=ownerComponent
+    @Operation(description = "Получение списка собственников по имени")
+    fun findByName(
+        @Parameter(description = "Имя собственника", example = "Лена", required = true,
+            style = ParameterStyle.DEFAULT)
+        @RequestBody name:String
+    )=ownerComponent
         .findByName(name)
         .map { owner ->OwnerResponse
         .getOwnerResponse(owner) }
 
     @GetMapping("/phone")
     @ResponseStatus(HttpStatus.FOUND)
-    fun findByPhone(@RequestBody phone: String) = OwnerResponse
+    @Operation(description = "Получение собственника по телефону")
+    fun findByPhone(
+        @Parameter(description = "Телефон собственника", example = "684-06-09", required = true,
+            style = ParameterStyle.DEFAULT)
+        @RequestBody phone: String
+    ) = OwnerResponse
         .getOwnerResponse(ownerComponent.findByPhone(phone))
 
     @GetMapping("/{id}/send")
     @ResponseStatus(HttpStatus.FOUND)
-    fun sendOwner(@PathVariable(name = "id") id: Long)=producerService
+    @Operation(description = "Отправка данных собственика")
+    fun sendOwner(
+        @Parameter(description = "Идентификатор собственника", example = "68406", required = true,
+            style = ParameterStyle.DEFAULT)
+        @PathVariable(name = "id") id: Long
+    )=producerService
         .produceOwner(ownerComponent.findById(id))
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody owner: Owner): Owner = ownerComponent.save(owner)
+    @Operation(description = "Создание собственника")
+    fun create(
+        @Parameter(description = "Данные  собственника", example = "68406", required = true,
+            style = ParameterStyle.DEFAULT)
+        @RequestBody owner: Owner
+    ): Owner = ownerComponent.save(owner)
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    fun update(@PathVariable(name = "id") id:Long, @RequestBody ownerRequest:OwnerRequest) {
+    @Operation(description = "Обновление собственника")
+    fun update(
+        @Parameter(description = "Идентификатор собственника", example = "68406", required = true,
+            style = ParameterStyle.DEFAULT)
+        @PathVariable(name = "id") id:Long,
+
+        @Parameter(description = "Идентификатор собственника", example = "68406", required = true,
+            style = ParameterStyle.DEFAULT)
+        @RequestBody ownerRequest:OwnerRequest
+    ) {
         var ownerFromTable = ownerComponent.findById(id)
         if(ownerRequest.gender.equals("MALE"))ownerFromTable.gender = Gender.MALE
         if(ownerRequest.gender.equals("FEMALE"))ownerFromTable.gender = Gender.FEMALE
@@ -69,7 +112,12 @@ class OwnerController(private val ownerComponent: OwnerComponent,
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    fun delete(@PathVariable(name = "id") id:Long) = ownerComponent.deleteById(id)
+    @Operation(description = "Удаление собственника")
+    fun delete(
+        @Parameter(description = "Идентификатор собственника", example = "68406", required = true,
+            style = ParameterStyle.DEFAULT)
+        @PathVariable(name = "id") id:Long
+    ) = ownerComponent.deleteById(id)
 }
 
 
