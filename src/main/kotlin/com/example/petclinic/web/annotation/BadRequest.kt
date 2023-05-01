84e478540ab3719005834e32b922bf5c5301a85e
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-05-01 18:28:56 +0500

    #t28 create annotation class BadRequest

---
+++ src/main/kotlin/com/example/petclinic/web/annotation/BadRequest.kt
@@ -0,0 +1,20 @@
+package com.example.petclinic.web.annotation
+
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.http.MediaType
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(
+    responseCode = "400",
+    description = "Bad Request",
+    content = [Content(
+        mediaType = MediaType.APPLICATION_JSON_VALUE)]
+)
+annotation class BadRequest {
+}
