31661944289f4ab598fb4ebb3f3aed20b7f0fc66
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2023-08-09 12:56:52 +0500

    #t32 update class PetComponentImplTest

--- src/test/kotlin/com/example/petclinic/db/services/PetComponentImplTest.kt
+++ src/test/kotlin/com/example/petclinic/db/services/PetComponentImplTest.kt
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
+@Transactional
 class PetComponentImplTest @Autowired constructor(val petComponent: PetComponent) {
 
     @Test
