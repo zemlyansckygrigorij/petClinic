b186a814977a69e1f762fe61a5b9beea4fdb60cf
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2024-02-07 14:29:11 +0500

    t36 - inherit owner and vet from person

--- src/main/kotlin/com/example/petclinic/db/entity/Owner.kt
+++ src/main/kotlin/com/example/petclinic/db/entity/Owner.kt
@@ -2,6 +2,8 @@ package com.example.petclinic.db.entity
 
 import java.util.*
 import jakarta.persistence.*
+import lombok.Getter
+import lombok.Setter
 
 /**
  * @author Grigoriy Zemlyanskiy
@@ -11,26 +13,14 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "owner",schema = "public")
 class Owner(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-     val id: Long,
-
-    @Column(name = "full_name", nullable = false)
-     var fullName: String,
-
-    @Column(name = "address", nullable = true)
-     var address: String,
-
-    @Column(name = "phone", nullable = true)
-     var phone: String,
-
-    @Column(name = "birthday", nullable = true)
-    @Temporal(TemporalType.DATE)
-     var birthday: Date,
-
-    @Column(name = "gender", nullable = true)
-    @Enumerated(EnumType.STRING)
-     var gender: Gender
-)
+    id: Long,
+    override var fullName: String,
+    override var address: String,
+    override var phone: String,
+    override var birthday: Date,
+    override var gender: Gender
+): Person( id,fullName,address,phone, birthday,gender) {
+    @Id  override var id: Long = id
+}
 
 
